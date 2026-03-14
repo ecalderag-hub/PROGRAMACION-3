77042b7cbb73c63b3589e37193ea7e0b1993af27
ecalderag-hub <ecalderag@miumg.edu.gt>
2026-03-14 01:18:36 -0600

    Versión 2 del programa para punto extra
    Modificación del programa para calcular cantidad, suma y promedio de los valores ingresados

--- TAREA_IV_LISTA_SIMPLE/programa_2_punto_extra.cpp
+++ TAREA_IV_LISTA_SIMPLE/programa_2_punto_extra.cpp
@@ -1,1 +1,66 @@
+#include <iostream>
+using namespace std;
 
+struct Nodo {
+    int dato;
+    Nodo* siguiente;
+};
+
+int main() {
+
+
+    Nodo* head = NULL;
+    Nodo* actual = NULL;
+
+    int valor;
+    char opcion = 's';
+
+    int cantidad = 0;
+    int suma = 0;
+
+
+    while(opcion == 's'){
+    cout << endl;
+    cout << "Ingrese un numero: ";
+    cin >> valor;
+
+        Nodo* nuevo = new Nodo;
+        nuevo->dato = valor;
+        nuevo->siguiente = NULL;
+
+        if(head == NULL) {
+            head = nuevo;
+            actual = nuevo;
+        } else {
+            actual->siguiente = nuevo;
+            actual = nuevo;
+        }
+          // CONTAR Y SUMAR
+        cantidad++;
+        suma += valor;
+
+        cout << "Desea ingresar otro numero? (s/n): ";
+        cin >> opcion;
+    }
+    cout << endl;
+
+    Nodo* temp = head;
+    while(temp != NULL) {
+        cout << temp->dato << " -> ";
+        temp = temp->siguiente;
+    }
+    cout << "NULL"<<endl;
+    cout<< "-------------------------------------" << endl;
+    cout << "NOMBRE: ERICKA DANIELA CALDERA GOMEZ" << endl;
+    cout<< "-------------------------------------";
+
+    float promedio = (float)suma / cantidad;
+
+    cout << "\nCantidad de nodos: " << cantidad;
+    cout << "\nSuma de valores: " << suma;
+    cout << "\nPromedio: " << promedio;
+
+    return 0;
+
+
+}
