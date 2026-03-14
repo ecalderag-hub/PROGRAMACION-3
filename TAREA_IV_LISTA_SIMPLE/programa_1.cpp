d560ab78ead118c17fe251fefe8c4397b87e2bb8
ecalderag-hub <ecalderag@miumg.edu.gt>
2026-03-14 00:53:26 -0600

    Programa principal
    Programa de lista enlazada en C++ que permite ingresar números, mostrar la lista, calcular cantidad de nodos, suma y promedio.

--- TAREA_IV_LISTA_SIMPLE/programa_1.cpp
+++ TAREA_IV_LISTA_SIMPLE/programa_1.cpp
@@ -1,1 +1,49 @@
+#include <iostream>
+using namespace std;
 
+struct Nodo {
+    int dato;
+    Nodo* siguiente;
+};
+
+int main() {
+
+    Nodo* head = NULL;
+    Nodo* actual = NULL;
+
+    int valor;
+    char opcion = 's';
+
+    while(opcion == 's'){
+    cout << "Ingrese un numero: ";
+    cin >> valor;
+
+        Nodo* nuevo = new Nodo;
+        nuevo->dato = valor;
+        nuevo->siguiente = NULL;
+
+        if(head == NULL) {
+            head = nuevo;
+            actual = nuevo;
+        } else {
+            actual->siguiente = nuevo;
+            actual = nuevo;
+        }
+        
+        cout << "Desea ingresar otro numero? (s/n): ";
+        cin >> opcion;
+    }
+
+    Nodo* temp = head;
+    while(temp != NULL) {
+        cout << temp->dato << " -> ";
+        temp = temp->siguiente;
+    }
+    cout << "NULL"<< endl;
+
+    cout << endl;
+    cout << "NOMBRE: ERICKA DANIELA CALDERA GOMEZ";
+
+    return 0;
+
+}
